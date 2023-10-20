79002d8b37b4b7d4c4e3845e22a08e04e0171c93
Sasha <sasha.shvets.2017@mail.ru>
2023-10-20 16:05:36 +0300

    dom rendering

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,10 @@ import { validatIp } from "./helpers";
 
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector("button");
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timezoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
 
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
@@ -12,7 +16,7 @@ function getData() {
       `https://geo.ipify.org/api/v2/country?apiKey=at_p01a9XrAcvXeCXdxw5RENkTp0a9kU&ipAddress=${ipInput.value}`
     )
       .then((responce) => responce.json())
-      .then(console.log);
+      .then((data) => setInfo(data));
   }
 }
 
@@ -20,4 +24,12 @@ function handleKey(e) {
   if (e.key === "Enter") {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText =
+    mapData.location.country + " " + mapData.location.region;
+  timezoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
