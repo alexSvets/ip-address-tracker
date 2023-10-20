8a9cf2ef313ccce10808e8a97430d20034531e8e
Sasha <sasha.shvets.2017@mail.ru>
2023-10-20 14:53:40 +0300

    handling user input

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-console.log("hello js");
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_p01a9XrAcvXeCXdxw5RENkTp0a9kU&${ipInput.value}`
+  )
+    .then((responce) => responce.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
